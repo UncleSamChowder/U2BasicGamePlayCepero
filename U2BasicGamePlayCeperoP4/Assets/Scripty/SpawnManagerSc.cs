644101358c41347a30ea80aa8d5f16fea6c2fda6
UncleSamChowder <745603@student.dusd.net>
2025-04-04 11:44:20 -0700

    g yhatrcvjiudfbukvjye
     vmsdl vmnheojkfgjg erlrtnglertnmgl;e rsd n ;klfavdfio;lkger '-[p0skg -reg erthlm pke pfrekg
    er[pgfkre :fkvfeP:gle
    [glr

--- U2BasicGamePlayCeperoP4/Assets/Scripty/SpawnManagerSc.cs
+++ U2BasicGamePlayCeperoP4/Assets/Scripty/SpawnManagerSc.cs
@@ -4,6 +4,8 @@ using UnityEngine;
 
 public class SpawnManagerSc : MonoBehaviour
 {
+    public GameObject[] animalPrefabs;
+    public int animalIndex;
     // Start is called before the first frame update
     void Start()
     {
@@ -13,6 +15,10 @@ public class SpawnManagerSc : MonoBehaviour
     // Update is called once per frame
     void Update()
     {
-        
+        if (Input.GetKeyDown(KeyCode.S))
+        {
+            Instantiate(animalPrefabs[animalIndex], new Vector3(0, 0, 20),
+                animalPrefabs[animalIndex].transform.rotation);
+        }
     }
 }
