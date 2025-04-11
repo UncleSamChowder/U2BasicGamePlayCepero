b9998859733fbb865547a6936f86188d46be5046
UncleSamChowder <745603@student.dusd.net>
2025-04-11 13:59:03 -0700

    rtghtht
    dfgh gfhgh

---
+++ U2BasicGamePlayCeperoP4/Assets/Challenge 2/Scripts/PlayerControllerX.cs
@@ -0,0 +1,22 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using Unity.VisualScripting;
+using UnityEngine;
+
+public class PlayerControllerX : MonoBehaviour
+{
+    public GameObject dogPrefab;
+    private float cooldown = 0;
+
+    // Update is called once per frame
+    void Update()
+    {
+        cooldown = cooldown - Time.deltaTime;
+        // On spacebar press, send dog
+        if (Input.GetKeyDown(KeyCode.Space)&& cooldown <= 0)
+        {
+            cooldown = 0.75f;
+            Instantiate(dogPrefab, transform.position, dogPrefab.transform.rotation);
+        }
+    }
+}
